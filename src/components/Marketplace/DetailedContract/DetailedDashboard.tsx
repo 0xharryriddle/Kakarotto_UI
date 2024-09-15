31eece2538165313efa0c4c27e0115f3c57baa56
Thai Cong Nguyen <21520169@gm.uit.edu.vn>
2024-09-15 16:31:47 +0700

    Fix some minor errors and build successfully

--- src/components/Marketplace/DetailedContract/DetailedDashboard.tsx
+++ src/components/Marketplace/DetailedContract/DetailedDashboard.tsx
@@ -9,203 +9,23 @@ import * as Enums from '@/utils/enum.util';
 import { getKakarottoCharacterAddress, getKakarottoItemAddress } from '@/contracts/utils/getAddress.util';
 import DetailedInformation from '@/components/Marketplace/DetailedContract/DetailedInformation';
 import DetailedOrder from '@/components/Marketplace/DetailedContract/DetailedOrder';
-import { Character, Item } from '@/interface/graphql.interface';
+import { Character, Item } from '@/generated/graphql';
 import { fetchCharacterData, fetchCharacterDataReturnType, fetchItemData, fetchItemDataReturnType } from '@/contracts/utils/fetchCardData.utill';
-
-const subgraph_url: string = process.env.NEXT_PUBLIC_ROOT_URI || ""
-
-const query = gql`{
-  characters {
-    characterAccount {
-      id
-      contractAddress
-    }
-    nft {
-      id
-      creator
-      owner {
-        address
-      }
-      tokenId
-      tokenURI
-      contractAddress
-      category
-      amount
-      rarity
-      orders {
-        id
-        category
-        tokenId
-        transactionHash
-        owner
-        buyer
-        price
-        status
-        createdAt
-        expiresAt
-        updatedAt
-      }
-      bids {
-        id
-        category
-        tokenId
-        bidder
-        seller
-        price
-        status
-        expiresAt
-        createdAt
-        updatedAt
-      }
-      activeOrder {
-        id
-        category
-        tokenId
-        transactionHash
-        owner
-        buyer
-        price
-        status
-        createdAt
-        expiresAt
-        updatedAt
-      }
-      searchOwner
-    }
-    attributes {
-      attribute
-      value
-    }
-    level
-    exp
-  }
-  treasureAccounts {
-    treasure {
-      tokenId
-      tokenURI
-      nft {
-        id
-        tokenId
-        tokenURI
-        contractAddress
-        category
-        amount
-        rarity
-        orders {
-          id
-          category
-          tokenId
-          transactionHash
-          owner
-          buyer
-          price
-          status
-          createdAt
-          expiresAt
-          updatedAt
-        }
-        bids {
-          id
-          category
-          tokenId
-          bidder
-          seller
-          price
-          status
-          expiresAt
-          createdAt
-          updatedAt
-        }
-        activeOrder {
-          id
-          category
-          tokenId
-          transactionHash
-          owner
-          buyer
-          price
-          status
-          createdAt
-          expiresAt
-          updatedAt
-        }
-      }
-    }
-    balance
-  }
-  items {
-    nft {
-      id
-      creator
-      owner {
-        address
-      }
-      tokenId
-      tokenURI
-      contractAddress
-      category
-      amount
-      rarity
-      orders {
-        id
-        category
-        tokenId
-        transactionHash
-        owner
-        buyer
-        price
-        status
-        createdAt
-        expiresAt
-        updatedAt
-      }
-      bids {
-        id
-        category
-        tokenId
-        bidder
-        seller
-        price
-        status
-        expiresAt
-        createdAt
-        updatedAt
-      }
-      activeOrder {
-        id
-        category
-        tokenId
-        transactionHash
-        owner
-        buyer
-        price
-        status
-        createdAt
-        expiresAt
-        updatedAt
-      }
-    }
-    attributes {
-      attribute
-      value
-      isIncrease
-      isPercentage
-    } 
-  }
-}`;
+import { querySubgraphs } from '@/services/graphql/subgraphs';
+import { client } from '@/graphql/client';
 
 interface DetailedDashboardProps {
   contractAddress: string,
   tokenId: string,
 }
 
-interface GraphQLData {
+interface GraphQLDataProps {
   characters: Character[];
   items: Item[];
 }
 
 export default function DetailedDashboard({ contractAddress, tokenId }: DetailedDashboardProps) {
-  const { chainId, address, isConnected } = useAccount();
+  const { chainId } = useAccount();
   const category = contractAddress === getKakarottoCharacterAddress(chainId ? chainId : 11155111) ? Enums.Categories.Character : contractAddress === getKakarottoItemAddress(chainId ? chainId : 11155111) ? Enums.Categories.Item : null;
 
   if (category == null) {
@@ -221,10 +41,10 @@ export default function DetailedDashboard({ contractAddress, tokenId }: Detailed
   const [characterData, setCharacterData] = useState<fetchCharacterDataReturnType>();
   const [itemData, setItemData] = useState<fetchItemDataReturnType>();
 
-  const { data: graphqlData, isLoading: graphqlIsLoading, error } = useQuery<GraphQLData>({
+  const { data: graphqlData, isLoading: graphqlIsLoading, error } = useQuery({
     queryKey: ['data'],
     async queryFn() {
-      return await request(subgraph_url, query);
+      return await querySubgraphs({ client });
     }
   });
 
@@ -234,7 +54,7 @@ export default function DetailedDashboard({ contractAddress, tokenId }: Detailed
         setLoading(true);
         setCharacterData(undefined);
         setItemData(undefined);
-        const { characters, items } = graphqlData;
+        const { characters, items } = graphqlData as GraphQLDataProps;
         await fetchNFTData({ characters: characters, category, items });
         setLoading(false);
       }
@@ -253,7 +73,7 @@ export default function DetailedDashboard({ contractAddress, tokenId }: Detailed
       category: Enums.Categories | null,
     }) => {
     try {
-      const dataResult = (category == Enums.Categories.Character ? await fetchCharacterData(characters) : Enums.Categories.Item ? await fetchItemData(items) : []).filter((data) => data.tokenId == tokenId)[0];
+      const dataResult = (category == Enums.Categories.Character ? await fetchCharacterData({characters}) : Enums.Categories.Item ? await fetchItemData({items}) : []).filter((data) => data.tokenId == tokenId)[0];
 
       category == Enums.Categories.Character ? setCharacterData(dataResult) : category == Enums.Categories.Item ? setItemData(dataResult) : [];
     } catch (error) {
